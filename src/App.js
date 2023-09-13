01ba7dfeec0971f932ace2245cfa372da4399d4d
Sajal <sajalkumarr030@gmail.com>
2023-09-13 16:38:44 +0530

    update changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 
+
 const App = () => {
   return (
     <>
@@ -10,13 +11,17 @@ const App = () => {
 
           </input>
         </div>
+        <div className='info'>
+          <h2 className='location'>
+            <i className="fa-solid fa-street-view"></i>
+          </h2>
+          <h2 className='location-name'>Alwar</h2>
+          <h1 className='temp'>5.25 Cel</h1>
+          <h3 className='tempmin_max'>
+          Min : 5.25 Cel | Max : 5.25 Cel
+          </h3>
+        </div>
       </div>
-
-    <div className='info'>
-      <h2 className='location'>
-      </h2>
-    </div>
-
     </>
   )
 }
