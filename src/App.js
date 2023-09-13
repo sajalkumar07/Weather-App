a77596433c3497de71e226493ebb6d69d01d2d8f
Sajal <sajalkumarr030@gmail.com>
2023-09-14 03:51:50 +0530

    update changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 
 const App = () => {
+  const [city, setCity]=useState(null)
+  const [search, setSearch]=useState("Mumbai")
+
+  useEffect(()=>{
+    const fetchApi = async()=>{
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=82fc7f32b34593886fa0a2471a2009b6`
+      const response = await fetch(url)
+      
+      const resJason = await response.json();
+      setCity(resJason)
+    }
+    fetchApi();
+  })
   return (
     <>
       <div className='box'>
         <div className='inputData'>
-          <input className='textArea' type='search' onChange={ (event) => {}}>
+          <input className='textArea' type='search' onChange={ (event) => {setSearch(event.target.value)}}>
           </input>
             <div className='wave wave-1'></div>
             <div className='wave wave-2'></div>
             <div className='wave wave-3'></div>
         </div>
+
+       {!city ? (
+        <p className='para'>No Data Found</p>
+       ):(
         <div className='info'>
           <h2 className='location'>
             <i className="fa-solid fa-street-view"></i>
           </h2>
-          <h2 className='location-name'>Alwar</h2>
-          <h1 className='temp'>5.25° Cel</h1>
-          <h3 className='tempmin_max'>
+          <h2 className='location-name'>{search}</h2>
+          <h1 className='temp'>{city.main.temp}</h1>
+          <h4 className='tempmin_max'>
           Min : 5.25° Cel | Max : 5.25° Cel
-          </h3>
+          </h4>
         </div>
+       )
+       }
+       
       </div>
     </>
   )
